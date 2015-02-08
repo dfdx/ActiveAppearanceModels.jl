838bbd9ef8e342f61ac9c251760197b07edeac8e
Andrei Zhabinski <a.zhabinski@adform.com>
2015-02-08 23:55:39 +0300

    playing around with calldump

--- src/calldump.jl
+++ src/calldump.jl
@@ -1,9 +1,33 @@
 
 const CALL_DUMP = {}
 
-macro calldump(expr)
-    if expr.head == :call
-        push!(CALL_DUMP, expr)
+
+function dump_wrapper(f)
+    function warped(args...)
+        println("$f: $args")
+        return f(args...)
     end
-    return expr    
+    return warped
+end
+
+macro calldump(expr::Expr)
+    if expr.head == :call
+        return :(dump_wrapper($(expr.args[1]), $(expr.args[2:end]...)))
+    else
+        return expr
+    end
+end
+
+
+bar(x) = x - 4
+foo(x) = 42 * bar(x)
+
+
+macro traverse(e::Expr, f::Function)
+    if e.head == :call
+        f(e.args[1])
+        e
+    else
+        e
+    end
 end
