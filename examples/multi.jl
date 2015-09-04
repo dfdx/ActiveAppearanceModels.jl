87e43cb8a79a479dd7ebeb5f0aa698ebd79023ae
dfdx <faithlessfriend@gmail.com>
2015-09-04 22:50:53 +0300

    showing initial shape in multi.jl

--- examples/multi.jl
+++ examples/multi.jl
@@ -12,7 +12,7 @@ for i=1:10
     try
         img_idx = rand(1:length(imgs))
         shape_idx = rand(1:length(imgs))        
-        # triplot(img, init_shape, m.trigs)
+        triplot(imgs[img_idx], shapes[shape_idx], m.wparams.trigs)
         @time fitted_shape, fitted_app = fit(m, imgs[img_idx], shapes[shape_idx], 30);
         triplot(imgs[img_idx], fitted_shape, m.wparams.trigs)
         println("Image #$img_idx; shape #$shape_idx")
